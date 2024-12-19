d1982b2ef75dfc5ee7026085835cbc04cefb1a50
naXXel <naXXera@gmail.com>
2024-12-19 21:42:29 +0700

    Итерация 2: Добавлена функция для поворота против часовой стрелки

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,2 +1,5 @@
 def rotate_matrix_right(matrix):
     return [list(reversed(col)) for col in zip(*matrix)]
+
+def rotate_matrix_left(matrix):
+    return [list(row) for row in list(zip(*matrix))[::-1]]
