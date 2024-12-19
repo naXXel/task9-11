8d2f2e8fed5ffdaf986967f8cc8ab2d835030948
naXXel <naXXera@gmail.com>
2024-12-19 21:43:09 +0700

    Итерация 4: Добавлена функция для суммы и разности больших чисел

--- Untitled-1.py
+++ Untitled-1.py
@@ -10,3 +10,12 @@ def sum_arrays(arr1, arr2):
     result = [a + b if a != b else 0 for a, b in zip(arr1, arr2)]
     result.sort()
     return result
+
+def big_number_operation(arr1, arr2, operation):
+    int_arr1 = int(''.join(map(str, arr1)))
+    int_arr2 = int(''.join(map(str, arr2)))
+    if operation == 'sum':
+        result = int_arr1 + int_arr2
+    elif operation == 'diff':
+        result = int_arr1 - int_arr2
+    return list(map(int, str(result)))
