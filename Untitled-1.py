bce91b0b07ebff992f4ac6e1219833cb9d9fb02d
naXXel <naXXera@gmail.com>
2024-12-19 21:42:50 +0700

    Итерация 3: Добавлена функция для суммы чисел из двух массивов

--- Untitled-1.py
+++ Untitled-1.py
@@ -3,3 +3,10 @@ def rotate_matrix_right(matrix):
 
 def rotate_matrix_left(matrix):
     return [list(row) for row in list(zip(*matrix))[::-1]]
+
+def sum_arrays(arr1, arr2):
+    arr1.sort(reverse=True)
+    arr2.sort()
+    result = [a + b if a != b else 0 for a, b in zip(arr1, arr2)]
+    result.sort()
+    return result
